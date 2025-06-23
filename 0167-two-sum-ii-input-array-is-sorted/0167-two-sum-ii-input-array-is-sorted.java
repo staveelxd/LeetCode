d78a4155eab787c33165cd2081a72703cad4d4a5
staveelxd <staveel@mail.ru>
2025-06-23 16:50:47 +0300

    Time: 0 ms (100%), Space: 47.2 MB (50.77%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,17 +1,30 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int s = 0, f = 1;
-        while (s < numbers.length) {
-            f = s + 1;
-            while (f < numbers.length) {
-                if (numbers[s] + numbers[f] == target) {
-                    return new int[]{s+1,f+1};
-                } else {
-                    f++;
-                }
-            }
-            s++;
+        int[] ret = new int[2];
+        bisearch(numbers, target, ret, 0, numbers.length - 1);
+        return ret;
+    }
+    private void bisearch(int[] numbers, int target, int[] ret, int l, int r) {
+        if (l==r)
+            return;
+        int sum = numbers[l] + numbers[r];
+        if (sum == target) {
+            ret[0] = l+1;
+            ret[1] = r+1;
+            return;
         }
-        throw new IllegalArgumentException("No solution");
+        int mid = l+(r-l)/2;
+        if (sum < target) {
+            if (numbers[mid] + numbers[r] <= target)
+                bisearch(numbers, target, ret, mid, r);
+            else
+                bisearch(numbers, target, ret, l+1, r);
+        }
+        else {
+            if (numbers[l] + numbers[mid] > target)
+                bisearch(numbers, target, ret, l, mid);
+            else
+                bisearch(numbers, target, ret, l, r-1);
+        }
     }
 }
