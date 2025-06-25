391fc1f115dafadf8daef0ec360f942feec3d265
staveelxd <staveel@mail.ru>
2025-06-25 13:04:41 +0300

    Time: 0 ms (100%), Space: 42.9 MB (66.42%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = 0;
+        if (target > nums[right]) return right+1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                ans = mid;
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
